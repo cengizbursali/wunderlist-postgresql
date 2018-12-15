ae1d45d70f6c493feca1f2843123a56b6bfce71a
cengizbursali <cengizbursali@users.noreply.github.com>
2018-12-15 19:17:18 +0300

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,19 +1,9 @@
 CREATE DATABASE mydatabase;
 CREATE USER myuser with password 'mypass';
-GRANT CONNECT ON DATABASE mydatabase TO myuser;
-GRANT CREATE ON DATABASE mydatabase TO myuser;
-GRANT CREATE ON SCHEMA public TO myuser;
-GRANT ALL PRIVILEGES ON SCHEMA public TO myuser;
-
+\c mydatabase;
 CREATE SCHEMA myschema;
-
-GRANT USAGE ON SCHEMA myschema TO myuser;
-GRANT ALL ON ALL TABLES IN SCHEMA myschema TO myuser;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA myschema TO myuser;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO myuser;
-
+ 
 drop table if exists myschema.user cascade;
-
 CREATE TABLE myschema.user(
   id varchar(255) not null,
   email varchar(255),
@@ -22,9 +12,8 @@ CREATE TABLE myschema.user(
   last_name varchar(255),
   PRIMARY KEY( id )
 );
-
+ 
 drop table if exists myschema.wunder cascade;
-
 CREATE TABLE myschema.wunder(
   id varchar(255) not null,
   title varchar(255),
@@ -34,13 +23,8 @@ CREATE TABLE myschema.wunder(
   modification_time date,
   PRIMARY KEY( id )
 );
-
-grant select on myschema.user to myuser;
-grant insert on myschema.user to myuser;
-grant update on myschema.user to myuser;
-grant delete on myschema.user to myuser;
-
-grant delete on myschema.wunder to myuser;
-grant select on myschema.wunder to myuser;
-grant insert on myschema.wunder to myuser;
-grant update on myschema.wunder to myuser;
+ 
+GRANT USAGE ON SCHEMA myschema TO myuser;
+GRANT ALL ON ALL TABLES IN SCHEMA myschema TO myuser;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA myschema TO myuser;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO myuser;
